155b608736bd2bd21ff9edac93ec9335d2206963
SANTIAGOLOZANO1947 <santiago2006lozano@gmail.com>
2025-09-09 00:14:00 -0500

    Add files via upload
    subiendo evidencia

---
+++ 06-week/taller estructura3/src/Ejercicio1.java
@@ -0,0 +1,13 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        ListaSimple lista = new ListaSimple();
+        lista.insertarFinal(10);
+        lista.insertarFinal(20);
+        lista.insertarFinal(30);
+        System.out.println("Lista después de insertar al final:");
+        lista.mostrar();
+    }
+}
+// AUTOR: SANTIAGO LOZANO MANTILLA
+// FECHA:08/09/2025
+// EJERCICIO1
---
+++ 06-week/taller estructura3/src/Nodo.java
@@ -0,0 +1,12 @@
+public class Nodo {
+    int dato;
+    Nodo siguiente;
+
+    public Nodo(int dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }
+}
+// AUTOR: SANTIAGO LOZANO MANTILLA
+// FECHA:08/09/2025
+// NODO
